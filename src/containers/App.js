616eb8b37642c5ccc89e16dcde34ede196a4b5d8
GamesByGeff <zgeffre@gmail.com>
2023-10-22 15:12:53 -0500

    updated with redux and react hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,29 @@
 import React, { useState, useEffect } from "react";
+import { connect } from 'react-redux';
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import './App.css'
 import Scroll from '../components/Scroll';
 
 
+import { setSearchField } from '../actions';
+
+const mapStateToProps = state => {
+    return {
+        searchField: state.searchField
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
+
 //STATE-the description of your app
 function App() {
     const [robots, setRobots] = useState([])
-    const [searchField, setSearchField] = useState('')
+    const [searchField, onSearchChange] = useState('')
     const [count, setCount] = useState(0)
 
     useEffect(() => {
@@ -17,25 +32,22 @@ function App() {
             .then(users => {setRobots(users)});
             console.log(count);
     },[count]) // only run if count changes
+           
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchField.toLowerCase());
+    })
+    return !robots.length ?
+        <h1>Loading</h1> :
+        (
+            <div className="tc">
+                <h1 className="f1">Robook</h1>
+                <button onClick={()=>setCount(count + 1)}>Click Me</button>
+                <SearchBox searchChange={onSearchChange} />
+                <Scroll>
+                        <CardList robots={filteredRobots} />                        
+                </Scroll>
+            </div>
+        );
+    }    
     
-    const onSearchChange = (event) => {
-        setSearchField(event.target.value)
-    }       
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchField.toLowerCase());
-        })
-        return !robots.length ?
-            <h1>Loading</h1> :
-            (
-                <div className="tc">
-                    <h1 className="f1">Robook</h1>
-                    <button onClick={()=>setCount(count + 1)}>Click Me</button>
-                    <SearchBox searchChange={onSearchChange} />
-                    <Scroll>
-                            <CardList robots={filteredRobots} />                        
-                    </Scroll>
-                </div>
-                );
-            }    
-    
-    export default App;
+    export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { legacy_createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
+import { searchRobots } from './reducers';
 import 'tachyons';
 
+const logger = createLogger();
+const store = legacy_createStore(searchRobots, applyMiddleware(logger));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 
 
 // If you want to start measuring performance in your app, pass a function
